fb48f063a471107f75a7d817e36d2d3652d17c54
ashoka <thapanvenkatesh1@gmail.com>
2019-09-01 16:46:47 +0530

    Adds validation to find credit card number for credit card related queries. TODO: 1. Fix structure things are still very hand-wavey. 2. Clean up some function code.

--- main.py
+++ main.py
@@ -22,28 +22,63 @@ class EchoWebSocket(tornado.websocket.WebSocketHandler):
         return
 
     def find_word(self,word,given_string):
-        preprocessed_word = word.strip()
-        raw_regex = re.compile(r"\b" + preprocessed_word + r"\b")
+        raw_regex = re.compile(r"\b" + word + r"\b", re.IGNORECASE)
+        incidences = raw_regex.findall(given_string)
+        return len(incidences)
 
+    def luhns_algorithm(self,card_number):
+        for i in range(len(card_number)-2,-1,-2):
+            sum_number = 0
+            modified_number = str(int(card_number[i])*2)
+            if(len(modified_number) > 1):
+                for each_alphabet in modified_number:
+                    sum_number += int(each_alphabet)
+            else:
+                sum_number = int(modified_number)
+            card_number = card_number[:i] + str(sum_number) + card_number[i+1:]
+        
+        sum_of_all_digits = 0
+        
+        for each_alphabet in card_number:
+            sum_of_all_digits += int(each_alphabet)
+
+        if(sum_of_all_digits%10 == 0):
+            return True
+        else:
+            return False
 
     def message_preprocessing(self,message):
-        print(message)
         if(self.name == None):
             self.name = message["user"]
-        return
+        if(self.credit_card == None):
+            credit_card_possibilities = ['credit card', 'CREDIT CARD', 'Credit Card', 'cReDiT CaRd','credit-card','credit - card','bank card','account card']
+            for each_word in credit_card_possibilities:
+                regex_result = self.find_word(each_word,message["message"])
+                if(regex_result>0):
+                    return 'You query requires your credit card number! Please input it!'
+            try:            
+                if(self.luhns_algorithm(message["message"]) == False):
+                    return 'Invalid card number! Please recheck!'
+                elif(self.luhns_algorithm(message["message"]) == True):
+                    self.credit_card = message["message"]
+                    return 'Thank you for entering your card number! Now please enter your query again so we can assist you!'
+            except ValueError:
+                pass
+        return 'It hit nothing'
 
-    def construct_response(self,message):
-        server_response = {}
-        server_response["user"] = "Server"
-        server_response["message"] = "haha,it works!"
-        return server_response
+    def prepare_message(self,preprocessed_message):
+        JSON_variable = {}
+        JSON_variable['user'] = 'Server'
+        JSON_variable['message'] = preprocessed_message
+        return JSON_variable
 
     def on_message(self, message):
-        server_response = self.construct_response(message)
         preprocessed_message = self.message_preprocessing(json.loads(message))
-        print(self.name)
+        final_message = self.prepare_message(preprocessed_message)
+        # print(self.name)
+        # print(self.credit_card)
         self.write_message(message)
-        self.write_message(server_response)
+        self.write_message(final_message)
 
     def on_close(self):
         print("WebSocket closed")
